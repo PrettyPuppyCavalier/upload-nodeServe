322672c30c370d4e9f5394fa61515878e4886b93
sand <prettypuppycavalier@gmail.com>
2024-04-14 23:19:56 +0800

    feat：  1，错误处理  2，上传文件处理

--- src/app/index.js
+++ src/app/index.js
@@ -14,19 +14,26 @@ const Mount = require('koa-mount')
 const Body = require('koa-body')
 const Router = require('../router')
 const app = new Koa()
+const errorhandler = require('../errorhandler/errorhandler')
 
-app.use(Body.koaBody())
+app.use(Body.koaBody({
+    multipart: true,
+    formidable: {
+        maxFileSize: 2000 * 1024 * 1024 // 设置最大文件大小为 200MB
+        /*uploadDir: Path.join(__dirname, '../../public/uploads'), // 设置上传文件的目录
+         keepExtensions: true, // 保持文件的扩展名
+         onProgress: (progress) => {
+         console.log('Upload Progress:', progress)
+         }*/
+    }
+}))
+
 app.use(Cors())
 app.use(Router.routes()).use(Router.allowedMethods())
 app.use(Mount('/static', Static(Path.join(__dirname, '../../public'))))
 
 // onerror和处理中间件选其一就可，如果都设置处理中间件会先处理，onerror不会触发，除非   ctx.app.emit('error', err, ctx)
 // 请求级别的错误处理
-app.on('error', (e, ctx) => {
-    console.error(e.stack)
-})
-
-
-
+app.on('error', errorhandler)
 
 module.exports = app
